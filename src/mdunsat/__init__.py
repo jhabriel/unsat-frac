0049555ba2fa591c345643fe4b376583eb456b7f
Jhabriel Varela <jhabriel@gmail.com>
2021-10-12 23:15:25 +0200

    Create __init__.py

---
+++ src/mdunsat/__init__.py
@@ -0,0 +1,3 @@
+from mdunsat.grids.grid_factory import GridFactory
+from mdunsat.constitutive_relationships import VanGenuchtenMualem, FractureVolume
+from mdunsat.ad_operators import FluxBaseUpwindAd, ParameterScalar
