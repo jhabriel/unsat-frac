a2ef20c2d3205c96412a30d60bbe428ca8302c30
Jhabriel Varela <jhabriel.varela@uib.no>
2022-03-02 09:17:27 +0100

    ENH: Extend features for more than one grid

--- src/mdunsat/ghost_variables.py
+++ src/mdunsat/ghost_variables.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import porepy as pp
 import numpy as np
 import scipy.sparse as sps
-from typing import Callable, Optional, Tuple, List, Any, Union, NewType, Literal
+from typing import Tuple, List, Union
 
 # Typing abbreviations
 Scalar = Union[int, float]
@@ -33,49 +33,63 @@ class GhostHydraulicHead:
     """
 
     def __init__(self, gb: pp.GridBucket, ghost_gb: pp.GridBucket):
+
+        # Physical grid bucket
         self._gb: pp.GridBucket = gb
+
+        # Ghost grid bucket
         self._ghost_gb: pp.GridBucket = ghost_gb
 
-        # Get the maximum dimension
-        self._maxdim: int = self._gb.dim_max()
+        # List grids
+        self._ghost_grid: List[pp.Grid] = [
+            g for g, _ in self._ghost_gb
+        ]
+
+        # List of lower-dimensional grids
+        self._ghost_low_dim_grids: List[pp.Grid] = [
+            g for g, _ in self._ghost_gb if g.dim < self._gb.dim_max()
+        ]
+
+        # List of  edges
+        self._ghost_edges: List[Edge] = [
+            e for e, _ in self._ghost_gb.edges()
+        ]
 
         # Get the list of ghost grids with g.dim >= (self._maxdim - 1).
-        ghost_grids: List[pp.Grid] = []
-        for g, _ in self._ghost_gb:
-            if g.dim >= self._maxdim - 1:
-                ghost_grids.append(g)
-        self._ghost_grids = ghost_grids
+        # ghost_grids: List[pp.Grid] = []
+        # for g, _ in self._ghost_gb:
+        #     if g.dim >= self._maxdim - 1:
+        #         ghost_grids.append(g)
+        # self._ghost_grids = ghost_grids
 
         # Get the list of ghost grids with g.dim == self._maxdim (ghost fracture grids).
-        ghost_frac_grids: List[pp.Grid] = []
-        for g, _ in self._ghost_gb:
-            if g.dim == self._maxdim - 1:
-                ghost_frac_grids.append(g)
-        self._ghost_frac_grids = ghost_frac_grids
+        # ghost_frac_grids: List[pp.Grid] = []
+        # for g, _ in self._ghost_gb:
+        #     if g.dim == self._maxdim - 1:
+        #         ghost_frac_grids.append(g)
+        # self._ghost_frac_grids = ghost_frac_grids
 
         # Get the list of edges with mg.dim == self._maxdim (ghost edges).
-        ghost_edges: List[Edge] = []
-        for e, d in self._ghost_gb.edges():
-            mg = d["mortar_grid"]
-            if mg.dim >= self._maxdim - 1:
-                ghost_edges.append(e)
-        self._ghost_edges = ghost_edges
+        # ghost_edges: List[Edge] = []
+        # for e, d in self._ghost_gb.edges():
+        #     mg = d["mortar_grid"]
+        #     if mg.dim >= self._maxdim - 1:
+        #         ghost_edges.append(e)
+        # self._ghost_edges = ghost_edges
 
-        # Get the ghost mortar projections.
-        ghost_mortar_proj: pp.ad.MortarProjections = pp.ad.MortarProjections(
-            gb=self._ghost_gb, grids=self._ghost_grids, edges=self._ghost_edges
+        # Ghost mortar projections
+        self._ghost_mortar_proj: pp.ad.MortarProjections = pp.ad.MortarProjections(
+            gb=self._ghost_gb, grids=self._ghost_grid, edges=self._ghost_edges
         )
-        self._ghost_mortar_proj = ghost_mortar_proj
 
-        # Get the ghost subdomain projections.
-        ghost_subdomain_proj: pp.ad.SubdomainProjections = pp.ad.SubdomainProjections(
-            grids=self._ghost_grids
+        # Ghost subdomain projections
+        self._ghost_subdomain_proj: pp.ad.SubdomainProjections = pp.ad.SubdomainProjections(
+            grids=self._ghost_grid
         )
-        self._ghost_subdomain_proj = ghost_subdomain_proj
 
-        # Get the elevation heads of the fracture grids by concatenating all ndarrays.
+        # Get the elevation heads of the lower-dimensional grids by concatenating all ndarrays
         self._cc: np.ndarray = np.concatenate(
-            [g.cell_centers[self._maxdim - 1] for g in ghost_frac_grids]
+            [g.cell_centers[self._gb.dim_max() - 1] for g in self._ghost_low_dim_grids]
         )
 
     def __repr__(self) -> str:
@@ -105,7 +119,7 @@ class GhostHydraulicHead:
     # Private methods
     def _proj_frac_hyd_head(self, h_frac: Union[AdArray, NonAd]) -> [AdArray, NonAd]:
 
-        pressure_threshold = -22.1  # this will have to be retrieved from a data dict
+        pressure_threshold: Scalar = -22.1  # this will have to be retrieved from a data dict
 
         if isinstance(h_frac, pp.ad.Ad_array):
             # Broadcast the fracture hydraulic head. The resulting ad operator will consist of
@@ -129,13 +143,13 @@ class GhostHydraulicHead:
             # ad_Array, but its Jacobian remains unchanged. Not sure about the repercusion
             # that this might have. But if we need to do things correctly, we should apply
             # something on the lines of the chunk from above :)
-            dry_cells: np.ndarray = hfrac_broad.val < (self._cc + pressure_threshold)
+            dry_cells: np.ndarray[bool] = hfrac_broad.val < (self._cc + pressure_threshold)
             hfrac_broad.val[dry_cells] = self._cc[dry_cells] + pressure_threshold
 
             # Now we are ready to project the hydraulic head onto the mortar grids. To this
             # aim, we first need the relevant subdomain projections and mortar projections.
             cell_prolongation = self._ghost_subdomain_proj.cell_prolongation(
-                grids=self._ghost_frac_grids
+                grids=self._ghost_low_dim_grids
             ).parse(gb=self._ghost_gb)
 
             secondary_to_mortar_avg = self._ghost_mortar_proj.secondary_to_mortar_avg.parse(
@@ -145,6 +159,7 @@ class GhostHydraulicHead:
             proj_h_frac: pp.ad.Ad_array = (
                 secondary_to_mortar_avg * cell_prolongation * hfrac_broad
             )
+
         else:
             # Check proper doc above
             broad_matrix: sps.lil_matrix = self._get_broadcasting_matrix()
@@ -152,7 +167,7 @@ class GhostHydraulicHead:
             dry_cells = hfrac_broad < (self._cc + pressure_threshold)
             hfrac_broad[dry_cells] = self._cc[dry_cells] + pressure_threshold
             cell_prolongation = self._ghost_subdomain_proj.cell_prolongation(
-                grids=self._ghost_frac_grids
+                grids=self._ghost_low_dim_grids
             ).parse(gb=self._ghost_gb)
             secondary_to_mortar_avg = self._ghost_mortar_proj.secondary_to_mortar_avg.parse(
                 self._ghost_gb
@@ -171,20 +186,20 @@ class GhostHydraulicHead:
         """
 
         # Create a list where item is number of cells corresponding to each frac grid
-        num_cells: List[int] = [g.num_cells for g in self._ghost_frac_grids]
+        num_cells: List[int] = [g.num_cells for g in self._ghost_low_dim_grids]
 
         # The number of rows of the broadcasting matrix corresponds to the total number of
         # fracture cells. The number of columns corresponds to the total number of
         # fractures, which must also coincide with the size of the active ad variable h_frac.
         rows: int = sum(num_cells)
-        cols: int = len(self._ghost_frac_grids)
+        cols: int = len(self._ghost_low_dim_grids)
         broad_matrix = sps.lil_matrix(np.zeros(shape=(rows, cols)))
 
         # Populate the matrix column-wise. The idea is to set to 1 the elements
         # corresponding to the number of ghost cells in each col, and leave the rest of the
         # elements untouched.
         for col in range(0, cols):
-            broad_matrix[col * num_cells[col] : (col + 1) * num_cells[col], col] = 1
+            broad_matrix[col * num_cells[col]: (col + 1) * num_cells[col], col] = 1
 
         return broad_matrix
 
